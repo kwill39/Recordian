f42e9d3725e218cb7dc816b3e8498456ad3fd112
kwilliams3 <kwill036@plattsburgh.edu>
2017-08-07 20:28:30 -0400

    Rename sample package

--- src/com/kylewill/Controller.java
+++ src/com/kylewill/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package com.kylewill;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
--- src/com/kylewill/Main.java
+++ src/com/kylewill/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package com.kylewill;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
