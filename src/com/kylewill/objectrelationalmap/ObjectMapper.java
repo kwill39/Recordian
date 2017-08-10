41f975f72b4ebafa64ed86d916da1ad8ad0cd2a7
Kyle <kwill036@plattsburgh.edu>
2017-08-10 11:47:24 -0400

    Remove ObjectMapper generic type
    ObjectMapper is the data access object interface which all data access
    objects implement. Since the data access objects only deal with
    DatabaseItem type objects, the generic type in ObjectMapper method
    parameters can be replaced with the DatabaseItem type.

--- src/com/kylewill/objectrelationalmap/ObjectMapper.java
+++ src/com/kylewill/objectrelationalmap/ObjectMapper.java
@@ -1,16 +1,18 @@
 package com.kylewill.objectrelationalmap;
 
+import com.kylewill.model.DatabaseItem;
+
 import java.util.List;
 
-interface ObjectMapper<T> {
+interface ObjectMapper {
     String url = "jdbc:sqlite:Hour_Tracker_Files/HourTracker.db";
 
     // Create a new record
-    void create(T t);
+    void create(DatabaseItem databaseItem);
     // Retrieve all records within a table
-    List<T> readAll();
+    List<DatabaseItem> readAll();
     // Update a record - a valid primary key must be available in the object passed
-    void update(T t);
+    void update(DatabaseItem databaseItem);
     //Delete a record - a valid primary key must be available in the object passed
-    void delete(T t);
+    void delete(DatabaseItem databaseItem);
 }
