1405002e41386970587ec967e195394d79841362
Kyle <kwill036@plattsburgh.edu>
2017-08-13 07:03:47 -0400

    Create Error dialog
    The purpose of the error dialog is to inform the user of errors that
    occur during the use of Kyle's Hour Tracker.

---
+++ src/com/kylewill/controller/ErrorController.java
@@ -0,0 +1,37 @@
+package com.kylewill.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ErrorController implements Initializable {
+    private Stage stage;
+    @FXML private Label errorLabel;
+    @FXML private Button okButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        okButton.setOnMouseClicked(event -> stage.close());
+    }
+
+    /**
+     * @param errorMessage a String that gets displayed to
+     *                     the user through errorLabel
+     */
+    public void setErrorMessage(String errorMessage) {
+        errorLabel.setText(errorMessage);
+    }
+
+    /**
+     * @param stage the JavaFX <code>stage</code> that contains the
+     *              view which this controller interacts with
+     */
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+}
