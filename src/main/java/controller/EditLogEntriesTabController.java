f51a35a719b66c5d8e18bd451f68d215c2a34d73
Kyle Williams <kwill036@plattsburgh.edu>
2017-09-11 02:42:20 -0400

    Finish the export log entries to PDF feature

--- src/main/java/controller/EditLogEntriesTabController.java
+++ src/main/java/controller/EditLogEntriesTabController.java
@@ -1,5 +1,9 @@
 package controller;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import databasemanagement.LogFileHelper;
 import databasemanagement.objectrelationalmap.LogEntryMapper;
 import javafx.collections.FXCollections;
@@ -24,10 +28,7 @@ import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringReader;
+import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -255,7 +256,7 @@ public class EditLogEntriesTabController implements Initializable {
 
         if (file != null) {
             try {
-                createPDF(file);
+                exportTableAsPDF(file);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -267,7 +268,6 @@ public class EditLogEntriesTabController implements Initializable {
      * new PDF using the {@link File} passed
      *
      * @param file a {@link File}
-     * @throws IOException
      */
     private void createPDF(File file) throws IOException {
 
@@ -356,5 +356,151 @@ public class EditLogEntriesTabController implements Initializable {
 
         document.save(file);
         document.close();
+    }
+
+    /**
+     * Creates a PDF displaying all log entries in a tabular
+     * format and saves the new PDF to the {@link File} passed
+     *
+     * @param outputFile the {@link File} that is to contain the PDF displaying all log entries
+     */
+    private void exportTableAsPDF(File outputFile) {
+        // PDF document
+        Document pdfDocument = new Document(PageSize.LETTER.rotate(), 60, 60, 60, 60);
+        try {
+            PdfWriter.getInstance(pdfDocument, new FileOutputStream(outputFile));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        pdfDocument.open();
+
+        // PDF table
+        PdfPTable pdfPTable = new PdfPTable(6);
+        pdfPTable.setWidthPercentage(100);
+        try {
+            pdfPTable.setWidths(new float[] {11, 7, 20.5f, 20.5f, 20.5f, 20.5f});
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+
+        // Add column header cells
+        Font columnHeaderFont = new Font(Font.FontFamily.HELVETICA, 13, 1);
+        PdfPCell dateCell = new PdfPCell(new Phrase("Date", columnHeaderFont));
+        PdfPCell hoursCell = new PdfPCell(new Phrase("Hours", columnHeaderFont));
+        PdfPCell commentsCell = new PdfPCell(new Phrase("Comments", columnHeaderFont));
+        PdfPCell locationCell = new PdfPCell(new Phrase("Location", columnHeaderFont));
+        PdfPCell companyNameCell = new PdfPCell(new Phrase("Company", columnHeaderFont));
+        PdfPCell supervisorCell = new PdfPCell(new Phrase("Supervisor", columnHeaderFont));
+
+        List<PdfPCell> tableCells = new ArrayList<>();
+        tableCells.add(dateCell);
+        tableCells.add(hoursCell);
+        tableCells.add(commentsCell);
+        tableCells.add(locationCell);
+        tableCells.add(companyNameCell);
+        tableCells.add(supervisorCell);
+
+        for (PdfPCell cell : tableCells) {
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            cell.setPaddingBottom(5);
+        }
+
+        pdfPTable.addCell(dateCell);
+        pdfPTable.addCell(hoursCell);
+        pdfPTable.addCell(commentsCell);
+        pdfPTable.addCell(locationCell);
+        pdfPTable.addCell(companyNameCell);
+        pdfPTable.addCell(supervisorCell);
+
+        // Used in populating the table
+        LogEntryMapper logEntryMapper = new LogEntryMapper();
+        List<LogEntry> logEntries = logEntryMapper.readAll();
+
+        // Sort the log entries by date in descending order
+        logEntries.sort((logEntry1, logEntry2) ->
+                logEntry1.getLogEntryDate().compareToIgnoreCase(logEntry2.getLogEntryDate()) * (-1));
+
+        // Used in determining the color of a row - either grey or white
+        // Starts with row 1 - an odd row
+        boolean oddRow = true;
+        BaseColor rowColor;
+        BaseColor whiteRow = BaseColor.WHITE;
+        BaseColor lightGreyRow = new BaseColor(225, 225, 225);
+
+        for (LogEntry logEntry : logEntries) {
+
+            // Ternary operator to check for null values within the log entry
+            // If there's a null value for a log entry's field, then the corresponding cell should be empty
+            dateCell = (logEntry.getLogEntryDate() == null) ? new PdfPCell() :
+                    new PdfPCell(new Phrase(logEntry.getLogEntryDate()));
+
+            hoursCell = (logEntry.getLogEntryHours() == null) ? new PdfPCell() :
+                    new PdfPCell(new Phrase(logEntry.getLogEntryHours()));
+
+            commentsCell = (logEntry.getLogEntryComments() == null) ? new PdfPCell() :
+                    new PdfPCell(new Phrase(logEntry.getLogEntryComments()));
+
+            // Build the location cell contents
+            // Some strings contain formatting characters added on to them such as a space
+            String locationName = (logEntry.getLogEntryLocationName() == null) ? "" :
+                    logEntry.getLogEntryLocationName();
+            String address = (logEntry.getLogEntryLocationAddress() == null) ? "" :
+                    System.lineSeparator() + logEntry.getLogEntryLocationAddress();
+            String city = (logEntry.getLogEntryLocationCity() == null) ? "" :
+                    System.lineSeparator() + logEntry.getLogEntryLocationCity() + " ";
+            String state = (logEntry.getLogEntryLocationState() == null) ? "" :
+                    logEntry.getLogEntryLocationState() + " ";
+            String zipCode = (logEntry.getLogEntryLocationZipCode() == null) ? "" :
+                    logEntry.getLogEntryLocationZipCode();
+            String locationCellContents = locationName + address + city + state + zipCode;
+            locationCell = new PdfPCell(new Phrase(locationCellContents));
+
+            companyNameCell = (logEntry.getLogEntryCompanyName() == null) ? new PdfPCell() :
+                    new PdfPCell(new Phrase(logEntry.getLogEntryCompanyName()));
+
+            // Build the supervisor cell contents
+            // Some strings contain formatting characters added on to them such as a space
+            String supervisorFirstName = (logEntry.getLogEntrySupervisorFirstName() == null) ? "" :
+                    logEntry.getLogEntrySupervisorFirstName() + " ";
+            String supervisorLastName = (logEntry.getLogEntrySupervisorLastName() == null) ? "" :
+                    logEntry.getLogEntrySupervisorLastName() + System.lineSeparator();
+            String supervisorDisplayName = (logEntry.getLogEntrySupervisorDisplayName() == null) ? "" :
+                    "(" + logEntry.getLogEntrySupervisorDisplayName() + ")";
+            String supervisorCellContents = supervisorFirstName + supervisorLastName + supervisorDisplayName;
+            supervisorCell = new PdfPCell(new Phrase(supervisorCellContents));
+
+            tableCells.clear();
+            tableCells.add(dateCell);
+            tableCells.add(hoursCell);
+            tableCells.add(commentsCell);
+            tableCells.add(locationCell);
+            tableCells.add(companyNameCell);
+            tableCells.add(supervisorCell);
+
+            rowColor = (oddRow) ? lightGreyRow : whiteRow;
+            // Alternate the color for the next row
+            oddRow = !oddRow;
+
+            for (PdfPCell cell : tableCells) {
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                cell.setPaddingBottom(5);
+                cell.setBackgroundColor(rowColor);
+            }
+
+            pdfPTable.addCell(dateCell);
+            pdfPTable.addCell(hoursCell);
+            pdfPTable.addCell(commentsCell);
+            pdfPTable.addCell(locationCell);
+            pdfPTable.addCell(companyNameCell);
+            pdfPTable.addCell(supervisorCell);
+        }
+        try {
+            pdfDocument.add(pdfPTable);
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        pdfDocument.close();
     }
 }
