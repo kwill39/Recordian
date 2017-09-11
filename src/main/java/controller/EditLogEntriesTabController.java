7143ada60e6ed1802bc9ee1aa7c336898fa1b05d
Kyle Williams <kwill036@plattsburgh.edu>
2017-09-11 03:27:08 -0400

    Remove code related to Hours Worked log file - closes #5
    The log file is no longer the primary method for displaying log entries
    to the user. This is now done through the pdf export feature which
    exports a pdf containing all log entries in a tabular format. The log
    file also no longer serves as the database for log entries. Instead,
    the SQLite database, which stores companies, locations and supervisors,
    now stores log entries created by the user. Therefore, there is no
    need to keep any code nor dependencies related to the generation of a
    log file nor the generation of a pdf of a log file.

--- src/main/java/controller/EditLogEntriesTabController.java
+++ src/main/java/controller/EditLogEntriesTabController.java
@@ -4,7 +4,6 @@ import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
-import databasemanagement.LogFileHelper;
 import databasemanagement.objectrelationalmap.LogEntryMapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -22,18 +21,13 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.util.converter.DefaultStringConverter;
 import model.LogEntry;
-import org.apache.commons.text.WordUtils;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
-import java.util.function.BiFunction;
 
 /**
  * @author  Kyle Williams
@@ -261,101 +255,6 @@ public class EditLogEntriesTabController implements Initializable {
                 e.printStackTrace();
             }
         }
-    }
-
-    /**
-     * Creates a PDF of the log file text and saves the
-     * new PDF using the {@link File} passed
-     *
-     * @param file a {@link File}
-     */
-    private void createPDF(File file) throws IOException {
-
-        BiFunction<PDDocument, PDPage, PDPageContentStream> prepareNewContentStream = (pdDocument, pdPage) -> {
-            try {
-                PDPageContentStream pdPageContentStream = new PDPageContentStream(pdDocument, pdPage);
-                pdPageContentStream.setFont(PDType1Font.HELVETICA, 12);
-                pdPageContentStream.setLeading(14.5);
-                pdPageContentStream.beginText();
-                int bodyX = 57;
-                int bodyY = 735;
-                pdPageContentStream.newLineAtOffset(bodyX, bodyY);
-                return pdPageContentStream;
-            } catch (IOException e) {
-                e.printStackTrace();
-                return null;
-            }
-        };
-
-        // Get logfile text
-        BufferedReader bufferedReader = new BufferedReader(new StringReader(LogFileHelper.getLogFileText()));
-
-        // Prepare the PDDocument, PDPage, and PDPageContentStream
-        PDDocument document = new PDDocument();
-        PDPage page = new PDPage();
-        PDPageContentStream contentStream = prepareNewContentStream.apply(document, page);
-
-        // Start writing to the PDF line by line
-        int lineCount = 0;
-        // If a line is longer than 40 characters, it is in danger
-        // of running off of the edge of the page.
-        int maximumLineLength = 40;
-        String aLineFromTheLogFile = bufferedReader.readLine();
-        if (aLineFromTheLogFile == null) {
-            // Create a blank page if the log file is empty
-            document.addPage(page);
-        }
-        while (aLineFromTheLogFile != null) {
-            // Use Apache Commons' WordUtils.wrap method to ensure that any line longer than
-            // the maximum line length gets wrapped before being written to the current PDF page.
-            String wrappedLines = WordUtils.wrap(aLineFromTheLogFile, maximumLineLength, null, true);
-            String[] linesToWrite = wrappedLines.split(System.lineSeparator());
-            for (String aLine : linesToWrite) {
-
-                /* If 49 lines have been written to the current page,
-                 * then the current page is considered to be full.
-                 * Close the current stream, add the current page to the
-                 * document, create a new page and a new stream,
-                 * and reset lineCount
-                 */
-                if (lineCount == 49) {
-                    contentStream.endText();
-                    contentStream.close();
-                    document.addPage(page);
-                    page = new PDPage();
-                    contentStream = prepareNewContentStream.apply(document, page);
-                    lineCount = 0;
-                }
-
-                // Write the line to the PDF
-                contentStream.showText(aLine);
-                lineCount++;
-                contentStream.newLine();
-            }
-
-            // Read the next line
-            aLineFromTheLogFile = bufferedReader.readLine();
-
-            /* The while loop exits when there's nothing left to read
-             * from the log file. Once this loop exits, it's expected
-             * that all pages have already been written to the document.
-             * Aside from this if statement, the only time I add the
-             * current page to the document is when the current page is full.
-             * With this if statement, I account for the case where
-             * the current page is not full but still has some text,
-             * and there's nothing left to read from the log file.
-             */
-            if ((aLineFromTheLogFile == null) && (lineCount > 0)) {
-                document.addPage(page);
-            }
-        }
-
-        // Ensure contentStream gets closed
-        contentStream.endText();
-        contentStream.close();
-
-        document.save(file);
-        document.close();
     }
 
     /**
