11d3e545ade1120a5be759d7dec8f92d60137184
Kyle Williams <kwill036@plattsburgh.edu>
2017-09-12 21:18:39 -0400

    Make pie chart option display company pie chart

--- src/main/java/controller/GraphsTabController.java
+++ src/main/java/controller/GraphsTabController.java
@@ -1,10 +1,14 @@
 package controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXRadioButton;
+import graph.companygraphgenerator.CompanyPieChartGenerator;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -13,8 +17,12 @@ import java.util.ResourceBundle;
 public class GraphsTabController implements Initializable {
     private Stage currentStage;
     private MainTabPaneController parentTabPaneController;
-    @FXML private ScrollPane graphContainer;
+    private PieChart companyPieChart = new CompanyPieChartGenerator().getPieChart();
     @FXML private ToggleGroup graphType;
+    @FXML private JFXRadioButton barGraphButton;
+    @FXML private JFXRadioButton pieChartButton;
+    @FXML private ScrollPane graphScrollPane;
+    @FXML private VBox graphContainer;
     @FXML private JFXButton generateReportButton;
 
     void setCurrentStage(Stage stage) {
@@ -27,6 +35,18 @@ public class GraphsTabController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        graphContainer.prefWidthProperty().bind(graphScrollPane.widthProperty());
+        graphContainer.prefHeightProperty().bind(graphScrollPane.heightProperty());
+        barGraphButton.setUserData("barGraph");
+        pieChartButton.setUserData("pieChart");
+        graphType.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            switch (newValue.getUserData().toString()) {
+                case "barGraph": break;
+                case "pieChart":
+                    graphContainer.getChildren().clear();
+                    graphContainer.getChildren().add(companyPieChart);
+                    break;
+            }
+        });
     }
 }
